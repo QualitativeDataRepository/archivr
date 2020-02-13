d3a2eddd0ab929fbcae57e91b4f07b81309d509c
Sebastian Karcher <karcher@u.northwestern.edu>
2020-02-13 11:53:47 -0500

    Add unit tests (#32)
    Improve documentation, clean up structure
    * add codecov

---
+++ tests/testthat/test-archivr.R
@@ -0,0 +1,64 @@
+context("Test archving core functionality")
+# We're only running the essential archiving functions on CRAN and Travis to not unecessarily spam the IA
+
+test_that("archiving a list of URLs in Wayback works", {
+  urls <- c("https://qdr.syr.edu", "https://cran.r-project.org/")
+# archive in Wayback machine
+  archivedURLs <- archiv(urls)
+  expect_true(is.data.frame((archivedURLs)))
+  expect_equal(nrow(archivedURLs), 2) # two URLs
+  expect_equal(ncol(archivedURLs), 4) # four observations
+  expect_true(as.logical(archivedURLs$available[1])) # Existing URL works fine
+})
+
+test_that("including a broken URL fails gracefully", {
+  skip_on_cran()
+  skip_on_travis()
+  urls <- c("https://qdr.syr.edu", "doesnotexist.r-project.org/")
+  # archive in Wayback machine
+  archivedURLs <- archiv(urls)
+  expect_true(is.data.frame((archivedURLs)))
+  expect_equal(nrow(archivedURLs), 2) # two URLs
+  expect_equal(ncol(archivedURLs), 4) # four observations
+  expect_true(as.logical(archivedURLs$available[1])) # Existing URL works fine
+  expect_false(as.logical(archivedURLs$available[2])) # non-existing URLs produce FALSE
+  
+})
+
+test_that("produces warning message for invalid URL", {
+  expect_message(archiv(c("doesnotexist.r-project.org")), "Discovered an error in saving the url")
+})
+
+test_that("archiving a single URL in Wayback works", {
+  skip_on_cran()
+  skip_on_travis()
+  archivedURL <- archiv_wayback("https://qdr.syr.edu")
+  expect_length(archivedURL, 4)
+  expect_true(as.logical(archivedURL[2])) # This should work
+})
+
+
+test_that("archiving from a URL in  Wayback works, including excepting URL ", {
+  skip_on_cran()
+  skip_on_travis()
+  archivedURLs <- archiv.fromUrl(
+    "https://www-cs-faculty.stanford.edu/~knuth/retd.html",
+    except="validator\\.w3\\.org|stanford"
+  )
+  expect_true(is.data.frame((archivedURLs)))
+  expect_equal(nrow(archivedURLs), 1) # one URL
+  expect_equal(ncol(archivedURLs), 4) # four variables
+  expect_true(as.logical(archivedURLs$available[1])) # this should work
+})
+
+# More relevant testing on this in test-extract.R
+test_that("archiving from a document works, including excepting URL patters", {
+  skip_on_cran()
+  skip_on_travis()
+  archivedURLs <- archiv.fromText("../data/testdoc.docx", except="doi\\.org\\/")
+  expect_true(is.data.frame((archivedURLs)))
+  expect_equal(nrow(archivedURLs), 3) # three URLs
+  expect_equal(ncol(archivedURLs), 4) # four observations
+  expect_true(as.logical(archivedURLs$available[1])) # this should work
+})
+
---
+++ tests/testthat/test-extract.R
@@ -0,0 +1,58 @@
+context("Test extracting from document")
+
+test_that("extracting URL from a string works", {
+  text <- "Lorem ipsum dolor www.example.com sit amet, consectetur adipiscing elit. Aenean a posuere ex. Ut viverra mattis ante a https://qdr.syr.edu venenatis. Sed elementum quis ipsum eu cursus. Curabitur dapibus augue in elementum lobortis. Duis vitae quam ipsum. Fusce maximus massa urna, sit amet finibus dui http://www.archive.org maximus ornare. Duis enim magna, sollicitudin aliquam auctor id, efficitur vel quam. Cras egestas luctus massa eu porta. Etiam metus neque, hendrerit eu ornare eu, porta sit amet elit. Suspendisse potenti. Aenean id tincidunt turpis."
+  urlList <- extract_urls_from_text(text)
+  expect_length(urlList, 3)
+  expect_equal(urlList[[1]], "www.example.com")
+})
+
+test_that("cleaning final characters in extracted URLs works", {
+  text <- "Lorem ipsum dolor www.example.com, sit amet, consectetur adipiscing elit. Aenean a posuere ex. Ut viverra mattis ante a <https://qdr.syr.edu> venenatis. Sed elementum quis ipsum eu cursus. Curabitur dapibus augue in elementum lobortis. Duis vitae quam ipsum. Fusce maximus massa urna, sit amet finibus dui (http://www.archive.org) maximus ornare. Duis enim magna, sollicitudin aliquam auctor id, efficitur vel quam. Cras egestas luctus massa eu porta. Etiam metus neque, hendrerit eu ornare eu, porta sit amet elit. Suspendisse potenti. Aenean id tincidunt turpis."
+  urlList <- extract_urls_from_text(text)
+  expect_length(urlList, 3)
+  expect_equal(urlList[[1]], "www.example.com")
+  expect_equal(urlList[[2]], "https://qdr.syr.edu")
+  expect_equal(urlList[[3]], "http://www.archive.org")
+})
+
+test_that("extracing from a .docx works, including except", {
+  urlList <- extract_urls_from_text("../data/testdoc.docx", except="doi\\.org\\/")
+  expect_length(urlList, 3)
+  expect_equal(urlList[[1]], "www.example.com")
+})
+
+test_that("extracing from a .pdf works, including except", {
+  urlList <- extract_urls_from_text("../data/testpdf.pdf", except="doi\\.org\\/")
+  expect_length(urlList, 3)
+  expect_equal(urlList[[1]], "www.example.com")
+})
+
+test_that("extracting from an XML file works", {
+  urlList <- extract_urls_from_text("../data/testxml.xml", except="doi\\.org\\/")
+  expect_length(urlList, 5)
+  expect_equal(urlList[[1]], "http://ifl.sagepub.com/content/42/4/292")
+})
+
+test_that("extracing from a .html files works, including except", {
+  urlList <- extract_urls_from_text("../data/testhtml.html", except="doi\\.org\\/")
+  expect_length(urlList, 2)
+  expect_equal(urlList[[1]], "http://www.example.com")
+})
+
+test_that("extracting from a URL works, including except", {
+  urlList <- extract_urls_from_webpage(
+        "https://www-cs-faculty.stanford.edu/~knuth/retd.html",
+        except="validator\\.w3\\.org")
+  expect_equal(urlList[1], "http://www-sul.stanford.edu")
+})
+
+test_that("extract from non-existing URL fails gracefully",{
+  skip_on_cran()
+  skip_on_travis()
+  urlList <- extract_urls_from_webpage(
+    "http://does-not-exist.r-project.org",
+    except="validator\\.w3\\.org")
+  expect_length(urlList, 0)
+})
+
